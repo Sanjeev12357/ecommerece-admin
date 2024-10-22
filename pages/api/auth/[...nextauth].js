40fcfa43df53e72ca98267a2d39b595bdbaab9e8
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2024-10-22 22:06:44 +0530

    third commit addeed oin enw admin

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "@/lib/mongodb"
 import GithubProvider from "next-auth/providers/github"
 
-const adminEmails = ['sanjeevsinghsaini48@gmail.com']
+const adminEmails = ['sanjeevsinghsaini48@gmail.com','adi6tnine@gmail.com']
 
 export const authOptions = {
   providers: [
